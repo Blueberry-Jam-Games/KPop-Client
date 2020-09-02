b0c8a3d7140f90d48be4bb875ada9271eee73604
Zenith08 <kaidenr@shaw.ca>
2020-09-01 21:27:02 -0700

    Initial Commit.

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+
+    public int puzzleID;
+    public PictureComponent aSide;
+    public PictureComponent bSide;
+    public string path = "http://127.0.0.1:5000/api-puzzle";
+    public string submission = "http://127.0.0.1:5000/api-response";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetPuzzleID());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator GetPuzzleID()
+    {
+        //Debug.Log("Preparing to send request");
+        UnityWebRequest www = UnityWebRequest.Get(path);
+        //Debug.Log("Yeilding to web request");
+        Debug.Log("Getting puzzle id");
+        yield return www.SendWebRequest();
+        //Debug.Log("Returned form yield");
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Got puzzle data");
+            Debug.Log(www.downloadHandler.text);
+            string[] obs = www.downloadHandler.text.Split(',');
+            string id = obs[0].Split('=')[1];
+            puzzleID = int.Parse(id);
+            aSide.LoadTexture(puzzleID);
+            bSide.LoadTexture(puzzleID);
+        }
+    }
+
+    public void SamePressed()
+    {
+        StartCoroutine(SubmitResponse("s"));
+    }
+
+    public void DifferentPressed()
+    {
+        StartCoroutine(SubmitResponse("d"));
+    }
+
+    IEnumerator SubmitResponse(string same)
+    {
+        //Debug.Log("Preparing to send request");
+        string response = submission + "?id=" + puzzleID + "&same=" + same;
+        UnityWebRequest www = UnityWebRequest.Get(response);
+        //Debug.Log("Yeilding to web request");
+        Debug.Log("Submitting response to");
+        yield return www.SendWebRequest();
+        //Debug.Log("Returned form yield");
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Submitted answer");
+            //nothing
+        }
+        Reload();
+    }
+
+    private void Reload()
+    {
+        SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
+    }
+}
---
+++ Assets/Scripts/PictureComponent.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class PictureComponent : MonoBehaviour
+{
+    Image display;
+    int index;
+    public string side;
+    public string root = "http://127.0.0.1:5000/api-image";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        display = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadTexture(int id)
+    {
+        index = id;
+        StartCoroutine(GetImage());
+    }
+
+    IEnumerator GetImage()
+    {
+        string path = root + "?id=" + index + "&side=" + side;
+
+        Debug.Log("Getting image side " + side + " path = " + path);
+
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(path);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Texture myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            display.sprite = Sprite.Create((Texture2D)myTexture, new Rect(0, 0, 512, 512), new Vector2());
+        }
+    }
+}
